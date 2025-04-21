284277e882ba981f9185243e9708f6d230f1b473
Spencer Steers <spencersteers@gmail.com>
2025-04-21 14:51:07 -0600

    q1 setup

--- keyboards/keychron/q1/rev_0100/keymaps/spencersteers/keymap.c
+++ keyboards/keychron/q1/rev_0100/keymaps/spencersteers/keymap.c
@@ -23,14 +23,9 @@
 // clang-format off
 
 enum custom_keycodes {
-#ifdef VIA_ENABLE
-    KC_MISSION_CONTROL = USER00,
-#else
     KC_MISSION_CONTROL = SAFE_RANGE,
-#endif
     KC_LAUNCHPAD
 };
-
 
 enum {
     TD_MEDIA_CONTROL = 0
@@ -45,28 +40,28 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [MAC_BASE] = LAYOUT_ansi_82(
-     KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,    KC_VOLD,  KC_VOLU,  KC_DEL,   TD(TD_MEDIA_CONTROL),//KC_INS,
+     KC_ESC,            KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,    KC_DEL,  TD(TD_MEDIA_CONTROL),
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_MEH,
      KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             MO(MAC_FN2),
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,    KC_UP,
      KC_HYPR,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RGUI,  MO(MAC_FN), KC_HYPR,  KC_LEFT,  KC_DOWN,  KC_RGHT
 ),
 
 [MAC_FN] = LAYOUT_ansi_82(
-     _______,            KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,   _______,  _______,
-     RGB_TOG,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,  RGB_M_SN, RGB_M_K,  RGB_M_X,  RGB_M_G,  RGB_M_T,  _______,  _______,    _______,  _______,            _______,
-     RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+     KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTL,   KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,    KC_VOLD,  KC_VOLU,   KC_DEL,  _______,
+     RGB_TOG,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,  RGB_M_SN, RGB_M_K,  RGB_M_X,  RGB_M_G,  RGB_M_T,  _______,  _______,    _______,  KC_DEL,             KC_PGUP,
+     RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            KC_PGDN,
      _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
      _______,            _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,
      _______,  _______,  _______,                                 _______,                                _______,  _______,    _______,  _______,  _______,  _______
 ),
 
 [MAC_FN2] = LAYOUT_ansi_82(
-     _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,    KC_VOLD,  KC_VOLU,  _______,  _______,
+     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            KC_PGUP,
-     _______,  _______,  KC_MS_U,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            KC_PGDN,
-     _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              KC_BTN1,            _______,
+     _______,  _______,  KC_MS_U,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            KC_PGDN,
+     _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              KC_BTN1,            _______,
      _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              KC_BTN1,  KC_WH_U,
      _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  KC_WH_L,  KC_WH_D,  KC_WH_R
 ),
@@ -113,7 +108,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // https://github.com/timmcca-be/qmk_firmware/commit/96e121fdc3f00381e254c3792902d6a517f76388
 bool encoder_update_user(uint8_t index, bool clockwise) {
+    // fn + encoder:   adjust connected display brightness
+    // fn2 + encoder:  adjust laptop display brightness
+    // encoder:        adjust volume 
     if (IS_LAYER_ON(MAC_FN)) {
+        if (clockwise) {
+            tap_code16(LCTL(KC_BRIU));
+        } else {
+            tap_code16(LCTL(KC_BRID));
+        }
+    } else if (IS_LAYER_ON(MAC_FN2)) {
         if (clockwise) {
             tap_code(KC_BRIU);
         } else {
